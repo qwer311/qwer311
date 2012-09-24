54fac50c67ad5010ac7e9c2a5ba7d7f9093147e6
qwer311 <biomedicalengineer@163.com>
2012-09-24 10:57:45 +0800

    new file: match.pl

---
+++ match.pl
@@ -0,0 +1,10 @@
+       #!/usr/bin/perl
+      while (<>) {                   # take one input line at a time
+           chomp;
+           if (/(https?:\/\/)?((?!\/).)+\//s) {
+                   
+                   print "Matched: |$`<$&>$'|\n";  # the special match vars
+                   } else {
+                       print "No match: |$_|\n";
+                   }
+       }
